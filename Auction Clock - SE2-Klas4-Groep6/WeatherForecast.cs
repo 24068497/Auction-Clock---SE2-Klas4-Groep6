0e139d1d5eef8f7a1885c84de45990d303e63a81
24068497 HHS - Onur Bicer <24068497@student.hhs.nl>
2025-10-01 10:19:04 +0200

    Add project files.

---
+++ Auction Clock - SE2-Klas4-Groep6/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Auction_Clock___SE2_Klas4_Groep6
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
