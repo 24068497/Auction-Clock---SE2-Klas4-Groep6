fc1237051d72e61e5d2acceca956b0b5955e97cd
24068497 HHS - Onur Bicer <24068497@student.hhs.nl>
2025-10-01 10:23:57 +0200

    Program edited to sheets

--- Auction Clock - SE2-Klas4-Groep6/Program.cs
+++ Auction Clock - SE2-Klas4-Groep6/Program.cs
@@ -1,36 +1,30 @@
+using Microsoft.OpenApi.Models;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace Auction_Clock___SE2_Klas4_Groep6
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddRouting();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
 }
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
