2226c368252ea34b111d3dadedf8859c22774749
Aditya200413 <24060682@student.hhs.nl>
2025-10-09 10:18:03 +0200

    companytest erbij

---
+++ Auction Clock - SE2-Klas4-Groep6/Controllers/CompanyTest.cs
@@ -0,0 +1,8 @@
+﻿namespace Auction_Clock___SE2_Klas4_Groep6.Controllers;
+
+public class CompanyTest
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+}
