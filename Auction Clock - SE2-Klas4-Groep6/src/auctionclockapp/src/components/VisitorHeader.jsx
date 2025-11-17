e5685af2bcebde258175a658253578221f5a445d
Timwerker <24001961@student.hhs.nl>
2025-11-17 23:41:16 +0100

    Header geupdated met nieuwe paginas

--- Auction Clock - SE2-Klas4-Groep6/src/auctionclockapp/src/components/VisitorHeader.jsx
+++ Auction Clock - SE2-Klas4-Groep6/src/auctionclockapp/src/components/VisitorHeader.jsx
@@ -4,34 +4,52 @@ import { Link } from 'react-router-dom';
 class VisitorHeader extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg bg-nav">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to='/'>
-                        <img src="../img/RoyalFloraHollandlogo.png" width="150px"></img>
+            <nav className="navbar navbar-expand-lg bg-nav">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to='/'>
+                        <img src="../img/RoyalFloraHollandlogo.png" width="150px" alt="Royal Flora Holland logo" />
                     </Link>
-                    
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item fs-5">
-                                <Link class="nav-link" to='/'>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link" to='/'>
                                     Home
                                 </Link>
                             </li>
 
+                            {/* 👇 Nieuwe pagina's toegevoegd */}
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link" to='/auctionClock/customer'>
+                                    Klok
+                                </Link>
+                            </li>
+
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link" to='/auctionClock/admin'>
+                                    Klok - Veiling Meester
+                                </Link>
+                            </li>
+
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link" to='/a'>
+                                    Product aanmaken
+                                </Link>
+                            </li>
                         </ul>
 
-                        <span class="navbar-text fs-5">
-                            <Link class="nav-link" to='/register'>
+                        <span className="navbar-text fs-5">
+                            <Link className="nav-link" to='/register'>
                                 Registreren
                             </Link>
                         </span>
 
-                        <span class="navbar-text fs-5">
-                            <Link class="nav-link" to='/login'>
+                        <span className="navbar-text fs-5">
+                            <Link className="nav-link" to='/login'>
                                 Inloggen
                             </Link>
                         </span>
@@ -39,7 +57,7 @@ class VisitorHeader extends React.Component {
                     </div>
                 </div>
             </nav>
-        )
+        );
     }
 }
 
