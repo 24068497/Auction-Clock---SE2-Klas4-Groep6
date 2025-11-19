7d0013c0789e80cdde6e4910325c986be37434e9
24068497 HHS - Onur Bicer <24068497@student.hhs.nl>
2025-11-19 11:20:09 +0100

    context toegevoegd

--- Auction Clock - SE2-Klas4-Groep6/Program.cs
+++ Auction Clock - SE2-Klas4-Groep6/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentityApiEndpoints<User>()
-    .AddEntityFrameworkStores<>();
+    .AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
